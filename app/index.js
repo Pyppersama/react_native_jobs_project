d1322e984d8f1db4d1f95e81818e2bf842ebd7e8
Offeh Godfrey <offehuzezi09@gmail.com>
2023-09-12 13:37:40 +0100

    home component basic layout

--- app/index.js
+++ app/index.js
@@ -15,10 +15,40 @@ const Home = () => {
     return (
         <SafeAreaView
             style ={{flex: 1, backgroundColor: COLORS.lightWhite}}
-        >
+        >       {/*allows d phone to not obstruct d app ui*/}
             <Stack.Screen 
-            
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,  //makes d header shadow invisible
+                    headerLeft: () =>(
+                        <ScreenHeaderBtn 
+                            iconUrl={icons.menu}
+                            dimension= "60%"
+                        />
+                    ),
+                    headerRight: () =>(
+                        <ScreenHeaderBtn 
+                            iconUrl={icons.profile}
+                            dimension= "100%"
+                        />
+                    ),
+                    headerTitle: ""
+                }}
             />
+            <ScrollView
+                showsVerticalScrollIndicator={false}    //makes the scrolling bar invisible
+            >
+                <View               //a.k.a div tag
+                    style= {{
+                        flex: 1, padding: SIZES.medium
+                    }}
+                >
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+
+            </ScrollView>
         </SafeAreaView>
     );
 }
